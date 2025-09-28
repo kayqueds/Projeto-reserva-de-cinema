2db8725671e6493c3f291575c69fee0887530b82
Kayque Estevão <kayque2911@gmail.com>
2025-09-28 19:27:29 -0300

    criando template da tela de cadastro

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,22 @@
-import { useState } from 'react'
-import './App.css'
-
+import "./App.css";
+import { BrowserRouter } from "react-router-dom";
+import RotasSite from "./routes/RotasSite";
+import Navigation from "./components/common/Navigation/Navigation";
 function App() {
- 
-
   return (
     <>
-      <div>
-      
-      </div>
+      <BrowserRouter>
+        <div className="app">
+          <header>
+            <Navigation />
+          </header>
+          <main>
+            <RotasSite />
+          </main>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/common/navigation/Navigation.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+function Navigation(){
+    return(
+        <nav>
+            <ul>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/cadastro">Cadastro</Link>
+                </li>
+                <li>
+                    <Link to="/login">Login</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navigation;
