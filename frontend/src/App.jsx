1cbd5feed317c213791e1126b59b77deefe2f830
Kayque Estevão <kayque2911@gmail.com>
2025-10-03 20:08:43 -0300

    adcionando o banner nos componentes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import RotasSite from "./routes/RotasSite";
 import Navigation from "./components/common/Navigation/Navigation";
 import Loading from "./pages/loading/Loading";
 import Footer from "./components/common/footer/Footer";
+import Mensagem from "./components/common/mensagem/Mensagem";
 
 function App() {
   const [showLoading, setShowLoading] = useState(true);
@@ -23,6 +24,7 @@ function App() {
           <Navigation />
           <main className="main-content">
             <RotasSite />
+            <Mensagem />
           </main>
           <Footer />
         </div>
